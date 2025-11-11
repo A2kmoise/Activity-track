6664a52e3b0a109d05305ad91c8f64a72ccc9c24
A2kmoise <abayomoise950@gmail.com>
2025-11-11 08:26:03 +0200

    -{ * * } - main 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main{
-    public static  void main(String[] args){
-        System.out.println("Print recent user activity in terminal");
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\n%60s%s%n", "", "================================ Github user activity getter ===================================");
+        Scanner c = new Scanner(System.in);
+        boolean  running = true;
+        while (running) {
+            System.out.println("\nMenu");
+            System.out.println("1)~{Github+CLI}~ Enter username");
+            System.out.println("2)~{Github+CLI}~ Exit");
+            System.out.println("Choose your option:");
+
+            String option = c.nextLine().trim();
+            switch (option) {
+                case "1" :
+                    System.out.println("Enter username here:");
+                    String subOption = c.nextLine();
+                    System.out.println("1) ~{Github+CLI}~ View commits");
+                    System.out.println("2) ~{Github+CLI}~ View push");
+                    break;
+                case "2":
+                    running = false;
+                default:
+                    System.out.println("Please choose the valid option");
+            }
+        }
     }
 }
