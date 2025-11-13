6d62f3c1377f704217842ee6c87b0b899bd0a2b4
A2kmoise <abayomoise950@gmail.com>
2025-11-13 06:26:16 +0200

    -{ * * }- main CLI

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
@@ -16,15 +15,44 @@ public class Main {
             switch (option) {
                 case "1" :
                     System.out.println("Enter username here:");
-                    String subOption = c.nextLine();
-                    System.out.println("1) ~{Github+CLI}~ View commits");
-                    System.out.println("2) ~{Github+CLI}~ View push");
-                    break;
+                    String username = c.nextLine().trim();
+
+                    boolean gitrunning = true;
+
+                    while (gitrunning) {
+
+                        String subOption = c.nextLine().trim();
+                        System.out.println("\nUser: " + username);
+                        System.out.println("1) ~{Github+CLI}~ View commits");
+                        System.out.println("2) ~{Github+CLI}~ View push");
+                        System.out.println("3) ~{Github+CLI}~ Go back");
+                        System.out.println("Choose your option:");
+
+                        switch(subOption) {
+                            case "1" :
+                                System.out.println("here you get commits");
+                                break;
+                            case "2" :
+                                System.out.println("here we get push");
+                                break;
+                            case "3":
+                                gitrunning = false;
+                                System.out.println("yep!");
+                                break;
+                            default:
+                                System.out.println("Please select a valid option");
+                        }
+                    }
+                 break;
+
                 case "2":
                     running = false;
+                    System.out.println("Goodbye!");
+                    break;
                 default:
                     System.out.println("Please choose the valid option");
             }
         }
+        c.close();
     }
 }
